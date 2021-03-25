77e79e833b5cff5ace89158884ceea3c9c193438
DavJosBer <boletindavid1995@gmail.com>
2021-03-25 16:47:50 +0000

    starWarsDataModeling

--- src/models.py
+++ src/models.py
@@ -8,26 +8,42 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password= Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__ = 'Planet'
+    id=Column(Integer, primary_key=True)
+    favorite_id=Column(Integer, ForeignKey('Favorite.id'))
+    name=Column(String(250), nullable=False)
+    climate=Column(String(250), nullable=False)
+    population=Column(Integer, nullable=False)
+    orbital_period=Column(Integer, nullable=False)
+    rotation_period=Column(Integer, nullable=False)
+    diameter=Column(Integer, nullable=False)
+
+class Character(Base):
+    __tablename__ = 'Character'
+    id=Column(Integer, primary_key=True)
+    favorite_id=Column(Integer, ForeignKey('Favorite.id'))
+    name=Column(String(250), nullable=False)
+    birth=Column(String(250), nullable=False)
+    gender=Column(String(250))
+    height=Column(Integer, nullable=False)
+    skin=Column(String(250), nullable=False)
+    eye=Column(String(250), nullable=False)
+
+class Favorite(Base):
+    __tablename__ = 'Favorite'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
+    user_id=Column(Integer, ForeignKey('User.id'))
+    planet_id=Column(Integer, ForeignKey('Planet.id'))
+    character_id=Column(Integer, ForeignKey('Character.id'))
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
